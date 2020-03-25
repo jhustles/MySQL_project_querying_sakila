b6feb7a733e49bdd7b64886e17515bc2ee9dee2d
rolex <36316219+jhustles@users.noreply.github.com>
2020-03-25 00:03:57 -0700

    Completed queries related to countries

--- sakila_db_queries.sql
+++ sakila_db_queries.sql
@@ -21,8 +21,23 @@ SELECT *
 FROM actor
 WHERE last_name LIKE '%GEN%';
 
-# Querying for actors whose last names contain the letters `LI`; ordered the rows by last name and first name, in that order:
+--  Querying for actors whose last names contain the letters `LI`; ordered the rows by last name and first name, in that order:
 SELECT *
 FROM actor
 WHERE last_name LIKE '%LI%'
 ORDER BY last_name, first_name;
+
+--   Querying for the country_id for countries IN Afghanistan, Bangladesh, and China
+SELECT country_id, country
+FROM country
+WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
+
+-- Adding a column to the table actor
+ALTER TABLE actor
+ADD description BLOB;
+
+-- # Deleting a column from the table actor using DROP
+ALTER TABLE actor
+DROP description;
+
+
